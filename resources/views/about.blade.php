11f3dc6ece3a033919c1f610f346b59bfbaaa0a8
Andhika Satriawan <andhika.satriawan1988@gmail.com>
2023-04-06 00:10:30 +0700

    Delete resources directory

--- resources/views/about.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.main')
-
-@section('content')
-    <h1>Halaman About</h1>
-    <h3>{{ $name }}</h3>
-    <p>{{ $email }}</p>
-    <img src="img/{{ $image }}" width="120" alt="{{ $name }}">
-@endsection
